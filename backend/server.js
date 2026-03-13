55f51f9c63b3648dcc83cb31e209c48f60c6386a
Amy <asb.asb.asbgfd@gmail.com>
2026-03-13 08:48:40 +0530

    abc added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,17 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-"mongodb+srv://asbasbasbgfd_db_user:<YoTO1QuboyShESNS>@cluster1.6r0t4vx.mongodb.net/?appName=Cluster1"
+"mongodb+srv://asbasbasbgfd_db_user:YoTO1QuboyShESNS@cluster1.6r0t4vx.mongodb.net/nutritionxp?retryWrites=true&w=majority"
 )
-.then(()=> console.log("MongoDB Atlas Connected"))
+.then(() => console.log("MongoDB Atlas Connected"))
 .catch(err => console.log(err));
+
+app.get("/", (req,res)=>{
+  res.send("Server running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`);
+});
